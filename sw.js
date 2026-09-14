95803413f5c86850a2873e1f302022f5f20afab5
dearself <shakaylahowell300@gmail.com>
2026-09-14 00:36:13 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,48 @@
-const CACHE = 'dearself-v42-6-beta';
-const APP_SHELL = ['./','./index.html','./version.json','./manifest.json'];
+const CACHE = 'dearself-v43-beta';
+const CORE = ['./', './index.html', './version.json', './manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE)
-      .then(cache => cache.addAll(APP_SHELL))
+      .then(cache => cache.addAll(CORE).catch(() => {}))
       .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('message', event => {
-  if(event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
-});
-
 self.addEventListener('fetch', event => {
-  if(event.request.method !== 'GET') return;
-  const url=new URL(event.request.url);
-
-  if(url.pathname.endsWith('/version.json')){
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.pathname.endsWith('/version.json') || url.pathname.endsWith('/index.html')) {
     event.respondWith(
-      fetch(event.request,{cache:'no-store'})
-        .then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(event.request,c));return r;})
-        .catch(()=>caches.match(event.request))
+      fetch(event.request, { cache: 'no-store' })
+        .then(response => {
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+          }
+          return response;
+        })
+        .catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
     );
     return;
   }
-
   event.respondWith(
     fetch(event.request)
-      .then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(event.request,c));return r;})
-      .catch(()=>caches.match(event.request))
+      .then(response => {
+        if (response && response.ok && url.origin === location.origin) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
