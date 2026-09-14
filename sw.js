9715ff0b99a69e2d26ba5ee7e7c122b447ce1233
dearself <shakaylahowell300@gmail.com>
2026-09-14 01:00:30 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dearself-v43-3-beta';
+const CACHE = 'dearself-v43-4-beta';
 const CORE = ['./', './index.html', './version.json', './manifest.json'];
 
 self.addEventListener('install', event => {
